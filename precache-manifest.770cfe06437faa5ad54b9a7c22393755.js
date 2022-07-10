f0ad8e16a8133f2edde3bc10c072b7c037282519
davitaro <davitaro@users.noreply.github.com>
2022-07-10 13:09:50 +0000

    deploy: b3ba9a49d612f4452f4457e93881f1bbf3979813

---
+++ precache-manifest.770cfe06437faa5ad54b9a7c22393755.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479c6ec234688e52f452ef5974ee7b61",
+    "url": "https://davitro.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "bcc6ba792488d317e753",
+    "url": "https://davitro.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "3677dedca5eb9e37f4c4",
+    "url": "https://davitro.github.io/task-management-frontend/static/js/2.2e61d798.chunk.js"
+  },
+  {
+    "revision": "bcc6ba792488d317e753",
+    "url": "https://davitro.github.io/task-management-frontend/static/js/main.3179ab33.chunk.js"
+  },
+  {
+    "revision": "5c403ee9a8f6684865db",
+    "url": "https://davitro.github.io/task-management-frontend/static/js/runtime~main.f9e7d76a.js"
+  }
+]);
